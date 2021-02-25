1d28e13aeda1c73b0b966167c8d3e1010378506f
Amol Ambkar <47270240+amolambkar@users.noreply.github.com>
2021-02-25 15:31:21 +0000

    Add files via upload

---
+++ 10_comments.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main(){
+    // this is a single line comment
+    fmt.Println("Single line comment is written by using // ")
+    
+    /*This
+    is
+    multiline 
+    comment */
+    
+    fmt.Println("Multiline Comment is written by using /* comment  */")
+
+
+}
